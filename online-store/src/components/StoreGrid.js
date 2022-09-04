b9095cfa85fb3b1f8882ac58d8c67eb3e4ea1441
Kiana Ortega <kianaorte@hotmail.com>
2022-09-04 13:08:32 +1000

    grid that is left aligned but flexes properly?

--- online-store/src/components/StoreGrid.js
+++ online-store/src/components/StoreGrid.js
@@ -5,15 +5,19 @@ import { Typography } from "@mui/material";
 
 export default function SpacingGrid() {
   return (
-    <Grid sx={{ flexGrow: 1 }} container spacing={2}>
+    <Grid
+      sx={{ flexGrow: 1, maxWidth: "1065px", display: "flex" }}
+      container
+      spacing={2}
+    >
       <Grid item xs={12}>
-        <Grid container justifyContent="center" spacing={2}>
-          {[0, 1, 2].map((value) => (
+        <Grid container rowSpacing={4} columnSpacing={{ xs: 2, sm: 4, md: 4 }}>
+          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((value) => (
             <Grid key={value} item>
               <Paper
                 sx={{
-                  height: 200,
-                  width: 250,
+                  minHeight: 200,
+                  minWidth: 250,
                   p: 3,
                   // backgroundColor: (theme) =>
                   // theme.palette.mode === "dark" ? "#1A2027" : "#fff",
