eae95d5933355a5cf066f0eba6d39289fa9fd72c
Chetan Takyar <takyarchetan@gmail.com>
2022-09-04 23:10:05 +1000

    Changed import grid to import grid2

--- online-store/src/components/StoreGrid.js
+++ online-store/src/components/StoreGrid.js
@@ -1,19 +1,19 @@
 import * as React from "react";
-import Grid from "@mui/material/Grid";
+import Grid2 from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import { Typography } from "@mui/material";
 
 export default function SpacingGrid() {
   return (
-    <Grid
-      sx={{ flexGrow: 1, maxWidth: "1065px", display: "flex" }}
+    <Grid2
+      sx={{ flexGrow: 1, maxWidth: "1065px", display: "flex"}}
       container
       spacing={2}
     >
-      <Grid item xs={12}>
-        <Grid container rowSpacing={4} columnSpacing={{ xs: 2, sm: 4, md: 4 }}>
+      <Grid2 item xs={12}>
+        <Grid2 container align = "center" rowSpacing={4} columnSpacing={{ xs: 2, sm: 4, md: 4 }}>
           {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((value) => (
-            <Grid key={value} item>
+            <Grid2 key={value} item>
               <Paper
                 sx={{
                   minHeight: 200,
@@ -25,10 +25,10 @@ export default function SpacingGrid() {
               >
                 <Typography>This is an item I am totally selling xd</Typography>
               </Paper>
-            </Grid>
+            </Grid2>
           ))}
-        </Grid>
-      </Grid>
-    </Grid>
+        </Grid2>
+      </Grid2>
+    </Grid2>
   );
 }
