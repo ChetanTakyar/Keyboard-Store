beaacd07b6a052630f0cd77350c72c36dded1864
Chetan Takyar <takyarchetan@gmail.com>
2022-09-05 22:05:45 +1000

    Refactored grid code

--- online-store/src/components/StoreGrid.js
+++ online-store/src/components/StoreGrid.js
@@ -1,34 +1,26 @@
 import * as React from "react";
-import Grid2 from "@mui/material/Grid";
+import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import { Typography } from "@mui/material";
 
 export default function SpacingGrid() {
   return (
-    <Grid2
-      sx={{ flexGrow: 1, maxWidth: "1800px", display: "flex" , alignItems:"center", justifyContent:"center"}}
-      container
-      spacing={2}
-    >
-      <Grid2 item xs={12}>
-        <Grid2 container alignItems= "center" justifyContent="center" rowSpacing={4} columnSpacing={{ xs: 2, sm: 4, md: 4 }}>
-          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((value) => (
-            <Grid2  key={value} item>
-              <Paper
-                sx={{
-                  minHeight: 200,
-                  minWidth: 250,
-                  p: 3,
-                  // backgroundColor: (theme) =>
-                  // theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-                }}
-              >
-                <Typography>This is an item I am totally selling xd</Typography>
-              </Paper>
-            </Grid2>
-          ))}
-        </Grid2>
-      </Grid2>
-    </Grid2>
+    <Grid container flexGrow={"1"} display={"flex"} rowSpacing={4} columnSpacing={{ xs: 2, sm: 4, md: 4 }}>
+    {Array.from(Array(24)).map((_, index) => (
+      <Grid item  alignItems={"center"} key={index}>
+          <Paper
+            sx={{
+              height: 250,
+              width: 200,
+              p: 3,
+            }}>
+              
+            <Typography>This is an item I am totally selling</Typography>
+          </Paper>
+      </Grid>
+      
+    ))}
+    
+    </Grid>
   );
 }
