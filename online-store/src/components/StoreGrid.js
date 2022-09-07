ffd6b0369909e099da9005f615d218f0ee27961d
Chetan Takyar <takyarchetan@gmail.com>
2022-09-08 09:06:28 +1000

    Added CardContent tags.

--- online-store/src/components/StoreGrid.js
+++ online-store/src/components/StoreGrid.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Grid from "@mui/material/Grid";
-import { Card, Typography } from "@mui/material";
+import { Card, CardContent, Typography } from "@mui/material";
 import '../App.css';
 
 
@@ -12,11 +12,11 @@ export default function SpacingGrid() {
       {Array.from(Array(22)).map((_, index) => (
         <Grid item xs={6} sm={4} md={3} flexGrow={1} alignItems={"center"} key={index}> 
             <Card
-              sx={{
-                p: 3,
-              }}>
+              sx={{p: 3,}}>
+              <CardContent>
                 
               <Typography>This is an item I am totally selling</Typography>
+              </CardContent>
             </Card>
         </Grid>
         
