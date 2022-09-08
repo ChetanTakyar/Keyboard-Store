cf0bd3fd7aa12b5694571d551fb1f6cc765b1731
Chetan Takyar <takyarchetan@gmail.com>
2022-09-08 17:05:03 +1000

    Added CardHeader and button, cleaned up imports

--- online-store/src/components/StoreGrid.js
+++ online-store/src/components/StoreGrid.js
@@ -1,29 +1,36 @@
 import * as React from "react";
 import Grid from "@mui/material/Grid";
-import { Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent, Typography, CardActions, CardHeader} from "@mui/material";
 import '../App.css';
+import Button from "@mui/material/Button";
+
+
+
 
 
 export default function SpacingGrid() {
   return (
     <div className= "Grid-Container">
-    
-      <Grid container flexDirection="row" justifyContent={"flex-start"} spacing={{ xs: 2, md: 3 }}>
-      {Array.from(Array(22)).map((_, index) => (
-        <Grid item xs={6} sm={4} md={3} flexGrow={1} alignItems={"center"} key={index}> 
-            <Card
-              sx={{p: 3,}}>
+      <Grid container flexDirection="row" justifyContent={"flex-start"} spacing={{ xs: 1, md: 2 }}>
+        {Array.from(Array(22)).map((_, index) => (
+          <Grid item xs={6} sm={4} md={3} flexGrow={1} alignItems={"center"} key={index}>
+            <Card sx={{p: 3}}>
               <CardContent>
-                
-              <Typography>This is an item I am totally selling</Typography>
+                <CardHeader title="Top Tier Keeb"/>
+                <Typography sx={{ fontSize: 12 }}>This is an item I am totally selling and it is very nice
+                </Typography>
+                <CardActions>
+                  <Button size="medium" fullWidth={true} variant="contained">Add to cart
+                  </Button>
+                </CardActions>
               </CardContent>
+
             </Card>
         </Grid>
         
       ))}
       
       </Grid>
-    
     </div>
   );
 }
