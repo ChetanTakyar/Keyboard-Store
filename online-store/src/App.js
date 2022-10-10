a28f180e3ab99f9e1283a0b00d4c0bf97870574c
Mark Berney <berneydwmark@gmail.com>
2022-10-10 14:19:50 +1300

    cleaned and refactored project

--- online-store/src/App.js
+++ online-store/src/App.js
@@ -1,15 +1,7 @@
 import React, { Component } from "react";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { createTheme, colors, ThemeProvider, CssBaseline } from "@mui/material";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import GroupBuy from "./pages/GroupBuy";
-import InStock from "./pages/InStock";
-import Sale from "./pages/Sale";
-import Contact from "./pages/Contact";
-
 class App extends Component {
   render() {
     return (
