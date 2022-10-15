fad9a681c47c7a964bc097620a4d64f705632600
Mark Berney <berneydwmark@gmail.com>
2022-10-15 21:44:26 +1300

    added ex. store grid to home page

--- online-store/src/App.js
+++ online-store/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 
-import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Home/>
       </div>
     );
   }
