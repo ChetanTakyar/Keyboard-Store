14472af79d683f705d9314288c80fc63ea6bf3af
Kiana Ortega <kianaorte@hotmail.com>
2022-10-15 20:29:38 +1100

    Linking working

--- online-store/src/App.js
+++ online-store/src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
+
 import Navbar from "./components/Navbar";
+//Pages
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import GroupBuy from "./pages/GroupBuy";
+
 class App extends Component {
   render() {
     return (
-          <div className="App">
-            <Navbar/>
-          </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/group-buy" element={<GroupBuy />} />
+          </Routes>
+        </div>
+      </Router>
     );
   }
 }
--- online-store/src/components/Navbar.js
+++ online-store/src/components/Navbar.js
@@ -1,40 +1,36 @@
 import { Component } from "react";
-import {
-  Button,
-  Typography,
-  AppBar,
-  Box,
-  Toolbar
-} from "@mui/material";
-
+import { Button, Typography, AppBar, Box, Toolbar } from "@mui/material";
+import { Link } from "react-router-dom";
 class Navbar extends Component {
   render() {
     return (
       <Box
         sx={{
-          flexGrow: 1
-        }}>
-
+          flexGrow: 1,
+        }}
+      >
         <AppBar>
           <Toolbar>
-            <Typography sx={{ flexGrow: 1 }}
-              variant="h6"
-              component="div">
-              Keeb City
-            </Typography>
-            <Button
-              variant="text"
-              color="inherit">
+            <Link to="/">
+              <Typography
+                sx={{ flexGrow: 1 }}
+                variant="h6"
+                component="div"
+                href="/"
+              >
+                Keeb City
+              </Typography>
+            </Link>
+            <Button variant="text" color="inherit" href="/store">
               Store
             </Button>
-            <Button
-              variant="text"
-              color="inherit">
-              Accessories
+            <Button variant="text" color="inherit" href="/group-buy">
+              Group Buy
             </Button>
-            <Button
-              variant="text"
-              color="inherit">
+            <Button variant="text" color="inherit" href="/contact">
+              Contact
+            </Button>
+            <Button variant="text" color="inherit" href="/login">
               Login
             </Button>
           </Toolbar>
