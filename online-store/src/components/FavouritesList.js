9e666c59613e2bd308805919eece9cf861a94e9f
Chetan Takyar <takyarchetan@gmail.com>
2023-04-18 15:28:41 +1000

    Creation of Favourites page and component files

---
+++ online-store/src/components/FavouritesList.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function FavouritesList() {
+  return <div></div>;
+}
---
+++ online-store/src/pages/Favourites.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+import { Typography } from "@mui/material";
+import FavouritesList from "../components/FavouritesList";
+
+export default class Favourites extends Component {
+  render() {
+    return <div></div>;
+  }
+}
