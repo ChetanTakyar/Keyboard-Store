c2dc50a2573091b10beed46040773183be37b706
Chetan Takyar <takyarchetan@gmail.com>
2023-04-18 15:07:08 +1000

    Fixed React out of scope bug

--- online-store/src/components/Navbar.js
+++ online-store/src/components/Navbar.js
@@ -1,7 +1,6 @@
 import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
-import { Component } from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import BasicModal from "./Modal.js";
 
 export default class Navbar extends Component {
   render() {
