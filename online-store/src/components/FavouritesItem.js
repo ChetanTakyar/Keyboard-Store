24f841b571d16d623a51ddc54e2b135937a246e5
Chetan Takyar <takyarchetan@gmail.com>
2023-04-19 16:19:39 +1000

    Formatted FavouritesItem Component

--- online-store/src/components/FavouritesItem.js
+++ online-store/src/components/FavouritesItem.js
@@ -1,15 +1,47 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import React from "react";
 
 export default function FavouritesItem() {
   return (
-    <Box flexGrow={1}>
+    <Box
+      display={"inline-flex"}
+      padding={"10px 0"}>
       <Box
-        flexDirection="row"
-        sx={{ display: "flex" }}>
-        <img src="https://m.media-amazon.com/images/I/61GnUc9e7VL._AC_SL1500_.jpg"></img>
-        <p>Amazon Keyboard</p>
-        <Button>Add to Cart</Button>
+        display="flex"
+        height="125px">
+        <img
+          id="favourites-image"
+          src="https://m.media-amazon.com/images/I/61GnUc9e7VL._AC_SL1500_.jpg"></img>
+        <Box
+          display="flex"
+          padding={"0 10px"}
+          flexDirection="column"
+          justifyContent="center">
+          <Typography
+            variant="h6"
+            component="div"
+            style={{ fontWeight: "bold" }}>
+            Amazon Keyboard
+          </Typography>
+          <Typography
+            variant="p"
+            component="div">
+            By Insert Company here
+          </Typography>
+          <Typography
+            variant="p"
+            component="div">
+            $5,000,000
+          </Typography>
+        </Box>
+        <Box
+          spacing={0}
+          padding={"0 10px"}
+          direction="column"
+          alignItems="center"
+          display="flex">
+          <Button variant="contained">Add to Cart</Button>
+        </Box>
       </Box>
     </Box>
   );
--- online-store/src/pages/Favourites.js
+++ online-store/src/pages/Favourites.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-
 import FavouritesItem from "../components/FavouritesItem";
 
 export default class Favourites extends Component {
