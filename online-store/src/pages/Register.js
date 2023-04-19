c82d953f5a68653431592dedda3f3b9789701bde
Mark Berney <berneydwmark@gmail.com>
2023-04-19 13:55:12 +1200

    added register page form

--- online-store/src/pages/Register.js
+++ online-store/src/pages/Register.js
@@ -1,4 +1,4 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, TextField, Typography } from "@mui/material";
 import React, { Component } from "react";
 
 export default class Register extends Component {
@@ -17,6 +17,40 @@ export default class Register extends Component {
             component="div">
             Register Page
           </Typography>
+          <Typography
+            variant="p"
+            component="div">
+            Enter your details to create an account with us.
+          </Typography>
+          <Typography
+            variant="p"
+            component="div">
+            Only your username will be shown to other people.
+          </Typography>
+          <TextField
+            id="name-register"
+            label="Name"
+            margin="normal"
+            variant="outlined"
+          />
+          <TextField
+            id="username-register"
+            label="Username"
+            margin="normal"
+            variant="outlined"
+          />
+          <TextField
+            id="email-register"
+            label="Email"
+            margin="normal"
+            variant="outlined"
+          />
+          <TextField
+            id="password-register"
+            label="Password"
+            margin="normal"
+            variant="outlined"
+          />
         </Grid>
       </div>
     );
