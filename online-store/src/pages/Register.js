43ce1aff4d3c16f321fc1c4771d079ea8d8981f7
Mark Berney <berneydwmark@gmail.com>
2023-04-19 13:51:14 +1200

    centered register page

--- online-store/src/pages/Register.js
+++ online-store/src/pages/Register.js
@@ -1,15 +1,23 @@
-import { Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import React, { Component } from "react";
 
 export default class Register extends Component {
   render() {
     return (
       <div>
-        <Typography
-          variant="h3"
-          component="div">
-          Register Page
-        </Typography>
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          style={{ minHeight: "100vh" }}>
+          <Typography
+            variant="h3"
+            component="div">
+            Register Page
+          </Typography>
+        </Grid>
       </div>
     );
   }
