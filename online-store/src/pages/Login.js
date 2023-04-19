91ea8b34c2899196344b2d766668c2e3adb0a7aa
Mark Berney <berneydwmark@gmail.com>
2023-04-19 13:05:58 +1200

    centered login form

--- online-store/src/pages/Login.js
+++ online-store/src/pages/Login.js
@@ -1,18 +1,26 @@
-import { TextField } from "@mui/material";
+import { Grid, TextField } from "@mui/material";
 import React, { Component } from "react";
 
 export default class Login extends Component {
   render() {
     return (
       <div>
-        <TextField
-          id="email-login"
-          label="Email"
-          variant="outlined"></TextField>
-        <TextField
-          id="password-login"
-          label="Password"
-          variant="outlined"></TextField>
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          style={{ minHeight: "100vh" }}>
+          <TextField
+            id="email-login"
+            label="Email"
+            variant="outlined"></TextField>
+          <TextField
+            id="password-login"
+            label="Password"
+            variant="outlined"></TextField>
+        </Grid>
       </div>
     );
   }
