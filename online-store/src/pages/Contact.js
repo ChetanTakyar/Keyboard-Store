cb4ec439d2ae3e8279652f2d1b73fc1fa633ff38
Mark Berney <berneydwmark@gmail.com>
2023-04-20 23:08:03 +1200

    centered contact page data

--- online-store/src/pages/Contact.js
+++ online-store/src/pages/Contact.js
@@ -1,20 +1,27 @@
 import React, { Component } from "react";
 
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 import ContactForm from "../components/ContactForm";
 
 export default class Contact extends Component {
   render() {
     return (
-      <div>
+      <Box
+        border="solid black 1px"
+        display="flex"
+        flexDirection="column"
+        height="100vh"
+        alignItems="center"
+        justifyContent="center"
+        textAlign="center">
         <Typography
           variant="h3"
           component="div">
           Contact
         </Typography>
         <ContactForm />
-      </div>
+      </Box>
     );
   }
 }
