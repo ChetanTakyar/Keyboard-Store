0fe1f6696f6a3af23025ff56b5548435b935daec
Mark Berney <berneydwmark@gmail.com>
2023-04-20 22:54:03 +1200

    split navbar by names and icons to left and right

--- online-store/src/components/Navbar.js
+++ online-store/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import {mdiAccount, mdiCartHeart } from "@mdi/js";
+import { mdiAccount, mdiCartHeart } from "@mdi/js";
 import Icon from "@mdi/react";
 import { AppBar, Box, Button, Toolbar } from "@mui/material";
 import React, { Component } from "react";
@@ -7,56 +7,68 @@ import { Link } from "react-router-dom";
 export default class Navbar extends Component {
   render() {
     return (
-      <Box
-        sx={{
-          flexGrow: 1,
-        }}>
+      <Box flexGrow="1">
         <AppBar position="static">
           <Toolbar>
-            <Button
-              variant="text"
-              color="inherit">
-              <Link
-                to="/"
-                style={{ color: "inherit", textDecoration: "inherit" }}>
-                Keeb City
-              </Link>
-            </Button>
-            <Button
-              variant="text"
-              color="inherit"
-              href="/store">
-              Store
-            </Button>
-            <Button
-              variant="text"
-              color="inherit"
-              href="/group-buy">
-              Group Buy
-            </Button>
-            <Button
-              variant="text"
-              color="inherit"
-              href="/contact">
-              Contact
-            </Button>
-            <Button
-              variant="text"
-              color="inherit"
-              href="/login">
-              <Icon
-                path={mdiAccount}
-                size={1}
-                color="inherit"
-              />
-            </Button>
-            <Button href="/favourites">
-              <Icon
-                path={mdiCartHeart}
-                size={1}
-                color="inherit"
-              />
-            </Button>
+            <Box
+              display="flex"
+              flexDirection="row"
+              flexGrow="1"
+              justifyContent="space-between">
+              {/* Left Side */}
+              <Box>
+                <Button
+                  variant="text"
+                  color="inherit">
+                  <Link
+                    to="/"
+                    style={{ color: "inherit", textDecoration: "inherit" }}>
+                    Keeb City
+                  </Link>
+                </Button>
+                <Button
+                  variant="text"
+                  color="inherit"
+                  href="/store">
+                  Store
+                </Button>
+                <Button
+                  variant="text"
+                  color="inherit"
+                  href="/group-buy">
+                  Group Buy
+                </Button>
+                <Button
+                  variant="text"
+                  color="inherit"
+                  href="/contact">
+                  Contact
+                </Button>
+              </Box>
+              {/* Right Side */}
+              <Box>
+                <Button
+                  variant="text"
+                  color="inherit"
+                  href="/login">
+                  <Icon
+                    path={mdiAccount}
+                    size={1}
+                    color="inherit"
+                  />
+                </Button>
+                <Button
+                  variant="text"
+                  color="inherit"
+                  href="/favourites">
+                  <Icon
+                    path={mdiCartHeart}
+                    size={1}
+                    color="inherit"
+                  />
+                </Button>
+              </Box>
+            </Box>
           </Toolbar>
         </AppBar>
       </Box>
