4f1d9a341df8c2e794db6167d9bb3e46d2ae08b0
Mark Berney <berneydwmark@gmail.com>
2023-04-21 11:20:25 +1200

    removed unnecessary code and uncentered storeGrid

--- online-store/src/components/StoreGrid.js
+++ online-store/src/components/StoreGrid.js
@@ -12,24 +12,20 @@ import {
   Typography,
 } from "@mui/material";
 import * as React from "react";
-import "../App.css";
 
 export default function SpacingGrid() {
   return (
-    <div className="grid-container">
+    <Box>
       <Grid
         container
         flexDirection="row"
         justifyContent={"flex-start"}
-        spacing={{ xs: 1, md: 2 }}>
+        spacing={{ xs: 1, md: 2 }}
+        xs={8}>
         {Array.from(Array(31)).map((_, index) => (
           <Grid
             item
-            xs={6}
-            sm={4}
-            md={4}
-            flexGrow={1}
-            alignItems={"center"}
+            xs={3}
             key={index}>
             <Card sx={{ p: 2 }}>
               <CardHeader title="Top Tier Keeb" />
@@ -40,17 +36,15 @@ export default function SpacingGrid() {
                   image="https://m.media-amazon.com/images/I/61GnUc9e7VL._AC_SL1500_.jpg"
                   alt="Keyboard"
                 />
-                <Typography sx={{ fontSize: 12 }}>
+                <Typography padding="10px 0">
                   This is an item I am totally selling and it is very nice
                 </Typography>
-                <Typography justifySelf={"right"}>$50,000</Typography>
+                <Typography>$50,000</Typography>
               </CardContent>
-
               <CardActions>
                 <IconButton aria-label="add to favorites">
                   <FavoriteIcon />
                 </IconButton>
-
                 <Button
                   size="medium"
                   fullWidth
@@ -62,6 +56,6 @@ export default function SpacingGrid() {
           </Grid>
         ))}
       </Grid>
-    </div>
+    </Box>
   );
 }
