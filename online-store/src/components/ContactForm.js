a3a65804832a509a1a89154399ac24808dd2efc6
Mark Berney <berneydwmark@gmail.com>
2023-04-20 23:13:40 +1200

    added form to contact page

--- online-store/src/components/ContactForm.js
+++ online-store/src/components/ContactForm.js
@@ -1,13 +1,45 @@
-import { Box } from "@mui/material";
+import { Box, Grid, TextField, Typography } from "@mui/material";
 import React, { Component } from "react";
 
 export default class ContactForm extends Component {
   render() {
     return (
       <Box>
-        <h1>Contact Us!</h1>
-        <p>Got any general enquiries or wanting to know when we restock an item? </p>
-        <p>Let us know in the form below! </p>
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="center">
+          <Typography
+            variant="p"
+            component="div">
+            Got any general enquiries or wanting to know when we restock an item?
+          </Typography>
+          <Typography
+            variant="p"
+            component="div">
+            Let us know in the form below!
+          </Typography>
+          <TextField
+            id="name-contact"
+            label="Name"
+            margin="normal"
+            variant="outlined"
+          />
+          <TextField
+            id="email-contact"
+            label="Email"
+            margin="normal"
+            variant="outlined"
+          />
+          <TextField
+            id="message-contact"
+            label="Message"
+            margin="normal"
+            variant="outlined"
+          />
+        </Grid>
       </Box>
     );
   }
--- online-store/src/pages/Contact.js
+++ online-store/src/pages/Contact.js
@@ -1,26 +1,27 @@
 import React, { Component } from "react";
 
-import { Box, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 
 import ContactForm from "../components/ContactForm";
 
 export default class Contact extends Component {
   render() {
     return (
-      <Box
-        border="solid black 1px"
-        display="flex"
-        flexDirection="column"
-        height="100vh"
-        alignItems="center"
-        justifyContent="center"
-        textAlign="center">
-        <Typography
-          variant="h3"
-          component="div">
-          Contact
-        </Typography>
-        <ContactForm />
+      <Box>
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          style={{ minHeight: "100vh" }}>
+          <Typography
+            variant="h3"
+            component="div">
+            Contact
+          </Typography>
+          <ContactForm />
+        </Grid>
       </Box>
     );
   }
