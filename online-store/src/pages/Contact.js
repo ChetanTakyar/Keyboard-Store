a051b93d05cfba50052fb015131eff6fbb960959
Mark Berney <berneydwmark@gmail.com>
2023-04-21 13:04:10 +1200

    converted Contact from class to functional component

--- online-store/src/pages/Contact.js
+++ online-store/src/pages/Contact.js
@@ -1,28 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 
 import { Box, Grid, Typography } from "@mui/material";
 
 import ContactForm from "../components/ContactForm";
 
-export default class Contact extends Component {
-  render() {
-    return (
-      <Box>
-        <Grid
-          container
-          spacing={0}
-          direction="column"
-          alignItems="center"
-          justifyContent="center"
-          style={{ minHeight: "100vh" }}>
-          <Typography
-            variant="h3"
-            component="div">
-            Contact
-          </Typography>
-          <ContactForm />
-        </Grid>
-      </Box>
-    );
-  }
-}
+const Contact = () => {
+  return (
+    <Box>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: "100vh" }}>
+        <Typography
+          variant="h3"
+          component="div">
+          Contact
+        </Typography>
+        <ContactForm />
+      </Grid>
+    </Box>
+  );
+};
+
+export default Contact;
