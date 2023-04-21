76b276221a3b40d98a3744f038cf41d9034ff98e
Mark Berney <berneydwmark@gmail.com>
2023-04-21 12:12:52 +1200

    gave back and improved storeGrid responsiveness

--- online-store/src/components/StoreGrid.js
+++ online-store/src/components/StoreGrid.js
@@ -1,6 +1,5 @@
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import {
-  Box,
   Button,
   Card,
   CardActions,
@@ -15,47 +14,53 @@ import * as React from "react";
 
 export default function SpacingGrid() {
   return (
-    <Box>
-      <Grid
-        container
-        flexDirection="row"
-        justifyContent={"flex-start"}
-        spacing={{ xs: 1, md: 2 }}
-        xs={8}>
-        {Array.from(Array(31)).map((_, index) => (
-          <Grid
-            item
-            xs={3}
-            key={index}>
-            <Card sx={{ p: 2 }}>
-              <CardHeader title="Top Tier Keeb" />
-              <CardContent>
-                <CardMedia
-                  component="img"
-                  height="194"
-                  image="https://m.media-amazon.com/images/I/61GnUc9e7VL._AC_SL1500_.jpg"
-                  alt="Keyboard"
-                />
-                <Typography padding="10px 0">
-                  This is an item I am totally selling and it is very nice
-                </Typography>
-                <Typography>$50,000</Typography>
-              </CardContent>
-              <CardActions>
-                <IconButton aria-label="add to favorites">
-                  <FavoriteIcon />
-                </IconButton>
-                <Button
-                  size="medium"
-                  fullWidth
-                  variant="contained">
-                  Add to cart
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </Box>
+    <Grid
+      container
+      flexDirection="row"
+      justifyContent={"flex-start"}
+      spacing={{ xs: 1, md: 2 }}
+      xs={12}
+      sm={10}
+      md={8}
+      lg={8}
+      xl={8}>
+      {Array.from(Array(31)).map((_, index) => (
+        <Grid
+          item
+          xs={12}
+          md={6}
+          lg={4}
+          xl={3}
+          key={index}>
+          <Card sx={{ p: 2 }}>
+            <CardHeader title="Top Tier Keeb" />
+            <CardContent>
+              <CardMedia
+                component="img"
+                height="auto"
+                image="https://m.media-amazon.com/images/I/61GnUc9e7VL._AC_SL1500_.jpg"
+                alt="Keyboard"
+                width="100%"
+              />
+              <Typography padding="10px 0">
+                This is an item I am totally selling and it is very nice
+              </Typography>
+              <Typography>$50,000</Typography>
+            </CardContent>
+            <CardActions>
+              <IconButton aria-label="add to favorites">
+                <FavoriteIcon />
+              </IconButton>
+              <Button
+                size="medium"
+                fullWidth
+                variant="contained">
+                Add to cart
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
   );
 }
