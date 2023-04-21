76e28d29848656d772de3a6b537ff35c0d916522
Mark Berney <berneydwmark@gmail.com>
2023-04-21 13:09:23 +1200

    converted Login from class to functional component

--- online-store/src/pages/Login.js
+++ online-store/src/pages/Login.js
@@ -1,47 +1,47 @@
 import { Box, Button, Grid, TextField, Typography } from "@mui/material";
-import React, { Component } from "react";
+import React from "react";
 
-export default class Login extends Component {
-  render() {
-    return (
-      <Box>
-        <Grid
-          container
-          spacing={0}
-          direction="column"
-          alignItems="center"
-          justifyContent="center"
-          style={{ minHeight: "100vh" }}>
-          <Typography
-            variant="h3"
-            component="div">
-            Login Screen
-          </Typography>
-          <Typography
-            variant="p"
-            component="div">
-            Enter your account details here to login.
-          </Typography>
-          <TextField
-            id="email-login"
-            label="Email"
-            margin="normal"
-            variant="outlined"
-          />
-          <TextField
-            id="password-login"
-            label="Password"
-            margin="normal"
-            variant="outlined"
-          />
-          <Button
-            variant="contained"
-            href="/register"
-            style={{ margin: "10px 0" }}>
-            Register
-          </Button>
-        </Grid>
-      </Box>
-    );
-  }
-}
+const Login = () => {
+  return (
+    <Box>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: "100vh" }}>
+        <Typography
+          variant="h3"
+          component="div">
+          Login Screen
+        </Typography>
+        <Typography
+          variant="p"
+          component="div">
+          Enter your account details here to login.
+        </Typography>
+        <TextField
+          id="email-login"
+          label="Email"
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+          id="password-login"
+          label="Password"
+          margin="normal"
+          variant="outlined"
+        />
+        <Button
+          variant="contained"
+          href="/register"
+          style={{ margin: "10px 0" }}>
+          Register
+        </Button>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Login;
