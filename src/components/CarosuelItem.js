fb4780c36dc621775fb9386d1a6f1f336bd836b3
Chetan Takyar <takyarchetan@gmail.com>
2023-05-04 20:13:29 +1000

    Created Carosuel Item component

---
+++ src/components/CarosuelItem.js
@@ -0,0 +1,12 @@
+import { Button, Paper } from "@mui/material";
+
+function Item(props) {
+  return (
+    <Paper>
+      <h2>{props.item.name}</h2>
+      <p>{props.item.description}</p>
+
+      <Button className="CheckButton">Check it out!</Button>
+    </Paper>
+  );
+}
