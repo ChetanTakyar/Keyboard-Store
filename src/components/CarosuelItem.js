e4dc144f245786389bea42194c8e80167cb8c7af
Chetan Takyar <takyarchetan@gmail.com>
2023-07-13 17:17:33 +1000

    Removed MUI from CarosuelItem

--- src/components/CarosuelItem.js
+++ src/components/CarosuelItem.js
@@ -1,17 +1,16 @@
-import { Paper } from "@mui/material";
 import { PropTypes } from "prop-types";
 import React from "react";
 
 function CarosuelItem({ item }) {
   return (
-    <Paper>
+    <div>
       <img
         src={item.image}
         alt={item.title}
         style={{ width: "50%", height: "30vw" }}
       />
       <h2>{item.title}</h2>
-    </Paper>
+    </div>
   );
 }
 CarosuelItem.propTypes = {
